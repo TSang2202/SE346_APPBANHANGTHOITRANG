a1ef84626d64d420565faa641665a7f61beb17ca
BichLoan <21521083@gm.uit.edu.vn>
2023-04-17 22:55:11 +0700

    Create Src

---
+++ src/CustomerView/constants/colors.js
@@ -0,0 +1,15 @@
+const CUSTOM_COLOR = {
+  //Tên màu: 'Mã màu',
+  ChathamsBlue: '#143F6B',
+  White: '#FFFFFF',
+  Black: '#000000',
+  Alto: '#D9D9D9',
+  CapeCod: '#3F4E4F',
+  SeaBuckthorn: '#F58D2D',
+  Carnation: '#F55353',
+  MineShaft: '#272727',
+  FlushOrange: '#FF8400',
+};
+
+export default CUSTOM_COLOR;
+
---
+++ src/Login_SignUp/assets/images/index.js
@@ -0,0 +1,7 @@
+// export {default as tênhình} from 'đường dẫn';
+
+export {default as IMG_PexelsPhotobyRioKuncoro} from './Intro/Pexels_Photo_by_Rio_Kuncoro.png';
+export {default as IMG_Rectangle2} from './Intro/Rectangle2.png';
+export {default as IMG_Pngtreetwittersocialmediaroundicon_87048231} from './Sign In/—Pngtree—twittersocialmediaroundicon_87048231.png';
+export {default as IMG_Facebook} from './Sign In/Facebook.png';
+export {default as IMG_google1} from './Sign In/google1.png';
