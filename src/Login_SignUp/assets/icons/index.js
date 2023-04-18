a8dda628e6efe2ec2131ab33817d8464034c60bb
BichLoan <21521083@gm.uit.edu.vn>
2023-04-18 13:21:33 +0700

    Screen

--- src/Login_SignUp/assets/icons/index.js
+++ src/Login_SignUp/assets/icons/index.js
@@ -1,7 +1,7 @@
 // export {default as tên icon} from 'đường dẫn';
 
-export {default as IC_Ellipse1} from './Welcome user/Ellipse1.png';
-export {default as IC_Ellipse2} from './Welcome user/Ellipse2.png';
+export {default as IC_Ellipse1} from './WelcomeUser/Ellipse1.png';
+export {default as IC_Ellipse2} from './WelcomeUser/Ellipse2.png';
 export {default as IC_Group} from './Group.png';
 export {default as IC_Show} from './Show.png';
 export {default as IC_Line} from './Intro/Line.png';
--- src/Login_SignUp/navigation/navigation.js
+++ src/Login_SignUp/navigation/navigation.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Congratulation from '../screens/Congratulation.js';
+import Done from '../screens/Done.js';
+import ForgotPassword from '../screens/ForgotPassword.js';
+import Intro from '../screens/Intro.js';
+import ResetPassword from '../screens/ResetPassword.js';
+import SignIn from '../screens/SignIn.js';
+import SignUp from '../screens/SignUp.js';
+import SmartOTP from '../screens/SmartOTP.js';
 import WelcomeUser from '../screens/WelcomeUser.js';
 
 const Stack = createNativeStackNavigator();
 function StackNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="Test"
+      initialRouteName="Intro"
       screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Congratulation" component={Congratulation} />
+      <Stack.Screen name="Done" component={Done} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <Stack.Screen name="Intro" component={Intro} />
+      <Stack.Screen name="ResetPassword" component={ResetPassword} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SmartOTP" component={SmartOTP} />
       <Stack.Screen name="WelcomeUser" component={WelcomeUser} />
     </Stack.Navigator>
   );
