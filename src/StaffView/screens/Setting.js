5c645ef1cc1d99a02c64483d135d473aced2d902
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-04-19 19:16:38 +0700

    CompleteOverView

---
+++ src/StaffView/screens/Setting.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+function Setting({navigation}){
+    return (
+      <View>
+        <Text>ViewShop1</Text>
+      </View>
+    )
+  }
+
+export default Setting
+
+const styles = StyleSheet.create({})
