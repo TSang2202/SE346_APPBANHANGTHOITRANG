4555a14b14ab48c47348576976b659eea396afad
BichLoan <21521083@gm.uit.edu.vn>
2023-04-20 22:15:24 +0700

    Sign in Done

--- src/Login_SignUp/screens/SignIn.js
+++ src/Login_SignUp/screens/SignIn.js
@@ -1,26 +1,97 @@
 import React, {useState} from 'react';
-import {StyleSheet, SafeAreaView, View} from 'react-native';
-import CUSTOM_COLOR from '../constants/colors.js';
-import FONT_FAMILY from '../constants/fonts.js';
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet, SafeAreaView, View, TouchableOpacity} from 'react-native';
+import HeaderWithBack from '../components/Header/HeaderWithBack.js';
+import CustomButton from '../components/Buttons/CustomButton.js';
+import TextInputCard from '../components/Cards/TextInputCard.js';
+import PasswordCard from '../components/Cards/PasswordCard.js';
+import HeaderTitlle from '../components/Header/HeaderTitlle.js';
+import {IMG_Facebook, IMG_google1, IMG_twitter} from '../assets/images/index.js';
+import HederContent from '../components/Header/HederContent.js';
 
 const SignIn = props => {
     const {navigation} = props;
     const [status, setStatus] = useState('');
     return (
         <SafeAreaView style={styles.container}>
+            <HeaderWithBack onPress={() => navigation.navigate('Intro')}></HeaderWithBack>
+            <View style={[styles.unitContainer, {height: '10%'}]}>
+                <HeaderTitlle title="Sign in"/>
+            </View>
+
+            <View style={[styles.unitContainer, styles.bodyContainer]}>
+                <View style={{flex: 2}}>
+                    <TextInputCard
+                        title="Phone number*"
+                        txtInput="03333333333"
+                    />
+                </View>
+
+                <View style={{flex: 2}}>
+                    <PasswordCard
+                        title="Pasword*"
+                        txtInput="********"
+                    />
+                </View>
+
+                <View style={{flex: 1, alignItems: 'flex-end'}}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
+                        <HederContent content="Forgot Password"></HederContent>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={styles.containerBot}>
+                <View style={styles.button}>
+                    <CustomButton
+                        type="primary"
+                        text="Sign in"
+                        onPress={() => navigation.navigate('SignIn')}
+                    />
+                </View>
+            </View>
+
+            <View style={[styles.unitContainer, styles.botContainer]}>
+                <View style={{flex: 5, justifyContent: 'center', alignItems: 'flex-end'}}>
+                    <HederContent content="Don't you have an account ? "></HederContent>
+                </View>
+                <View style={{flex: 2, justifyContent: 'center', alignItems: 'flex-start'}}>
+                    <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+                        <HederContent content="Sign up"></HederContent>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </SafeAreaView>
     );
 };
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+container: {
+    flex: 1,
+},
+unitContainer: {
+    width: '80%',
+    marginHorizontal: '10%',
+    justifyContent: 'center',
+},
+bodyContainer: {
+    height: '40%',
+    top: '5%',
+    flexDirection: 'column',
+},
+containerBot: {
+    width: '100%',
+    height: '10%',
+    bottom: '-5%',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+button: {
+    width: '100%',
+    height: '100%',
+},
+botContainer: {
+    height: '15%',
+    bottom: '-3%',
+    flexDirection: 'row',
+}
 });
 export default SignIn;
