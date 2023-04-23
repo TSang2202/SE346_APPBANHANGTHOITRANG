f9e454cab36a218d04c2407638bc61eb0bff2e74
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-04-23 23:13:12 +0700

    updateSetting

--- src/StaffView/screens/Setting.js
+++ src/StaffView/screens/Setting.js
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native'
 import React from 'react'
-
-function Setting({navigation}){
+import { Acount } from './OverView'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import CUSTOM_COLOR from '../constants/colors'
+function Setting(){
     return (
-      <View>
-        <Text>ViewShop1</Text>
+      <SafeAreaView>
+      <View style = {{width: '100%', height: 150, backgroundColor: CUSTOM_COLOR.LavenderBlush}}>
+        <TouchableWithoutFeedback style = {styles.buttonchage}
+          onPress={()=>{}}
+        >
+          <Text style = {{fontStyle: 'italic'}}>Change Your BackgroundColor</Text>
+        </TouchableWithoutFeedback>
       </View>
+      </SafeAreaView>
     )
   }
 
+const styles = StyleSheet.create({
+  buttonchage:{
+    width: 50,
+    height: 20,
+    backgroundColor: CUSTOM_COLOR.White,
+    borderColor: CUSTOM_COLOR.Black,
+    borderRadius: 9,
+    marginTop: 10,
+    borderWidth: 5
+  }
+})
 export default Setting
-
-const styles = StyleSheet.create({})
