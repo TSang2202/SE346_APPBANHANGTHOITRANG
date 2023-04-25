4e6a85e9686984a834a9c279496a688336ffc774
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-04-26 03:25:04 +0700

    updateTumLum

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import MainNavigator from './src/Login_SignUp/navigation/navigation.js';
 import type {PropsWithChildren} from 'react';
-
+import StackNavigator from './src/StaffView/navigation/navigation.js';
+import Setting from './src/StaffView/screens/Setting.js';
+import ViewShop2 from './src/StaffView/screens/ViewShop2.js';
+import Chat from './src/StaffView/screens/Chat.js';
 function App(): JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#FFF'}}>
-      <MainNavigator></MainNavigator>
-    </SafeAreaView>
+    <StackNavigator></StackNavigator>
   );}
 
 const styles = StyleSheet.create({
