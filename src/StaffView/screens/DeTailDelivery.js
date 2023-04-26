c93dfe40986d9bdebeb368dab6d074fc3180cb9c
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-04-27 04:21:07 +0700

    updateScr

---
+++ src/StaffView/screens/DeTailDelivery.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function DeTailDelivery() {
+  return (
+    <View>
+      <Text>DeTailDelivery</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/StaffView/screens/EditProduct.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function EditProduct() {
+  return (
+    <View>
+      <Text>edit-addproduct</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
