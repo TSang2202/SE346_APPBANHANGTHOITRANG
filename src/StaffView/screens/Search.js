d20d5406497fcd0635b0619b32346b9c3a185683
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-04-27 23:41:43 +0700

    completeSearch

--- src/StaffView/screens/Search.js
+++ src/StaffView/screens/Search.js
@@ -1,10 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Search from '../components/Search'
-export default function Search({navigation}) {
+import MyProduct1 from '../components/MyProductOne'
+import { Data } from './MyProduct'
+export default function SearchSrc({navigation}) {
   return (
     <View>
-     <Search></Search>
+     <Search
+        placeholder = 'Search In The Shop'
+     ></Search>
+      <MyProduct1
+              source = {Data[0].pic}
+              title = {Data[0].name}
+              price = {Data[0].price}
+              soluongtonkho = {Data[0].ware}
+              soluonglove = {Data[0].love}
+              soluongview = {Data[0].view}
+              soluongban = {Data[0].sold}
+              edit = {()=>navigation.navigate('EditProduct')}
+              ></MyProduct1>
     </View>
   )
 }
