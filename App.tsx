1d6e32c051c3cc0cae1804629e26a6df0381f42b
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-04-28 15:01:50 +0700

    updateToMerge

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import MainNavigator from './src/Login_SignUp/navigation/navigation.js';
 import type {PropsWithChildren} from 'react';
-import StackNavigator from './src/StaffView/navigation/navigation.js';
-import Order from './src/StaffView/screens/Order.js';
-import MyProduct from './src/StaffView/screens/MyProduct.js';
+
 function App(): JSX.Element {
   return (
-   <StackNavigator></StackNavigator>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#FFF'}}>
+      <MainNavigator></MainNavigator>
+    </SafeAreaView>
   );}
 
 const styles = StyleSheet.create({
