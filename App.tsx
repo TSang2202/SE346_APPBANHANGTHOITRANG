3216de6df0151cca90eaf45aba364abccd3685bd
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-04-28 22:24:42 +0700

    update

--- App.tsx
+++ App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import MainNavigator from './src/Login_SignUp/navigation/navigation.js';
 import type {PropsWithChildren} from 'react';
-
+import StackNavigator from './src/StaffView/navigation/navigation.js';
 function App(): JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#FFF'}}>
-      <MainNavigator></MainNavigator>
-    </SafeAreaView>
+    <StackNavigator></StackNavigator>
   );}
 
 const styles = StyleSheet.create({
