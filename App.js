96707c6224e76b9c3e2c880dc30208c37e2ccdc8
TSang2202 <21522546.gm.uit.edu.vn>
2023-05-24 22:50:16 +0700

    update

--- App.js
+++ App.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 import { SafeAreaView, StyleSheet } from 'react-native';
 import MainNavigator from './src/Login_SignUp/navigation/navigation.js';
-import type { PropsWithChildren } from 'react';
+//import type { PropsWithChildren } from 'react';
 
 import CustomerBottomTab from './src/CustomerView/navigation/CustomerBottomTab.js';
 import OverView from './src/StaffView/screens/OverView.js';
 import StackNavigator from './src/StaffView/navigation/navigation.js';
 
 const App = () => {
-    return (
+  return (
 
-        <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF' }}>
-            <StackNavigator />
-        </SafeAreaView>
-    );
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF' }}>
+      <StackNavigator />
+    </SafeAreaView>
+  );
 }
 
 
