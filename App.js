1523f5e40130c4563308873975220a496b55028d
TSang2202 <21522546.gm.uit.edu.vn>
2023-05-31 08:55:30 +0700

    update

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const App = () => {
   return (
 
     <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF' }}>
-      <StackNavigator />
+      <CustomerBottomTab />
     </SafeAreaView>
   );
 }
