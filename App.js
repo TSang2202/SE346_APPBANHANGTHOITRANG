49a2714fba86fb5a34daae66045aa22101f510ee
TSang2202 <21522546.gm.uit.edu.vn>
2023-06-02 15:45:48 +0700

    Ham lay du lieu

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import {firebase} from './Firebase/firebase';
+import React, { useState, useEffect } from 'react';
+import { firebase, Firestore } from './Firebase/firebase';
 import MainNavigator from './src/Login_SignUp/navigation/navigation';
 import CustomerBottomTab from './src/CustomerView/navigation/CustomerBottomTab';
+import { doc, getDoc } from "firebase/firestore";
 
 function App() {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
+
+  const [dataUser, setDataUser] = useState()
+
+  const getDataUser = async (userId) => {
+    const docRef = doc(Firestore, "users", userId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      setDataUser(docSnap.data())
+    } else {
+      console.log("No such document!");
+    }
+  }
 
   function onAuthStateChanged(user) {
     setUser(user);
@@ -14,8 +29,19 @@ function App() {
     }
   }
 
+
+
   useEffect(() => {
     const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+
+    if (user) {
+      getDataUser(firebase.auth().currentUser.uid)
+      console.log(dataUser)
+
+    }
+
+
+
     return subscriber;
   }, []);
 
@@ -26,6 +52,7 @@ function App() {
   if (!user) {
     return <MainNavigator />;
   }
+
 
   return <CustomerBottomTab />;
 }
