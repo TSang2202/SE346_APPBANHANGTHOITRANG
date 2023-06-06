62a0ccaae691c87501b6e7771cf199326b3ccfc3
Thương Lê Đăng <ledangthuong2003@gmail.com>
2023-06-06 19:05:29 +0700

    Xin đấy, đừng bị gradle nữa

--- src/CustomerView/screens/SearchingFunctions.js
+++ src/CustomerView/screens/SearchingFunctions.js
@@ -1,30 +1,19 @@
-import firebase from 'firebase/app';
-import firestore from '@react-native-firebase/firestore';
-
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from 'firebase/storage'
 const firebaseConfig = {
   apiKey: "AIzaSyCIaaa_hRxppBZrDoXlxFNFP4nDFxbN0o8",
   authDomain: "shoppingapp-ada07.firebaseapp.com",
-  databaseURL: "https://shoppingapp-ada07-default-rtdb.firebaseio.com",
   projectId: "shoppingapp-ada07",
   storageBucket: "shoppingapp-ada07.appspot.com",
   messagingSenderId: "373805724422",
-  appId: "1:373805724422:web:551b87a21de185ee212487",
-  measurementId: "G-321BTX96JN"
+  appId: "1:373805724422:web:e20a70e818ac7117212487",
+  measurementId: "G-LB7SNNSDMZ"
 };
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-const db = firestore();
-//Tìm kiếm sản phẩm theo tên
-const searchProductByName = async (name) => {
-  const productsRef = db.collection('SANPHAM');
-  const query1 = productsRef.collection.where()
-  const query = productsRef.where('TenSP', '>=', name).where('TenSP', '<=', name + '\uf8ff');
-  const querySnapshot = await query.get();
-  const result = [];
-  querySnapshot.forEach((doc) => {
-    result.push(doc.data());
-  });
-  return result;
-};
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const db = getDatabase(FirebaseApp);
+export const Firestore = getFirestore(FirebaseApp);
+export const Storage = getStorage(FirebaseApp);
