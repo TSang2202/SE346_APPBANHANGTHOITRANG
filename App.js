76a706c09f7a9ef4f2fb6418dc00ceb1c6926aa5
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-06-07 21:08:49 +0700

    123

--- App.js
+++ App.js
@@ -6,63 +6,67 @@ import CustomerBottomTab from './src/CustomerView/navigation/CustomerBottomTab';
 import StackNavigator from './src/StaffView/navigation/navigation';
 import { doc, getDoc } from 'firebase/firestore';
 import { NavigationContainer } from '@react-navigation/native';
-
+import User from './src/StaffView/screens/User';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
+  // const [initializing, setInitializing] = useState(true);
+  // const [user, setUser] = useState();
 
 
-  const [dataUser, setDataUser] = useState();
+  // const [dataUser, setDataUser] = useState();
 
-  const getDataUser = async (userId) => {
-    const docRef = doc(Firestore, "NGUOIDUNG", userId);
-    const docSnap = await getDoc(docRef);
+  // const getDataUser = async (userId) => {
+  //   const docRef = doc(Firestore, "NGUOIDUNG", userId);
+  //   const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-      console.log('Document data:', docSnap.data());
-      setDataUser(docSnap.data());
-    } else {
-      console.log('No such document!');
-    }
-  };
+  //   if (docSnap.exists()) {
+  //     console.log('Document data:', docSnap.data());
+  //     setDataUser(docSnap.data());
+  //   } else {
+  //     console.log('No such document!');
+  //   }
+  // };
 
-  function onAuthStateChanged(user) {
-    setUser(user);
-    if (initializing) {
-      setInitializing(false);
-    }
-  }
+  // function onAuthStateChanged(user) {
+  //   setUser(user);
+  //   if (initializing) {
+  //     setInitializing(false);
+  //   }
+  // }
 
-  useEffect(() => {
-    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+  // useEffect(() => {
+  //   const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
 
-    if (user) {
-      getDataUser(firebase.auth().currentUser.uid)
-      console.log(dataUser)
+  //   if (user) {
+  //     getDataUser(firebase.auth().currentUser.uid)
+  //     console.log(dataUser)
 
-    }
+  //   }
 
-    return subscriber;
-  }, [user]);
+  //   return subscriber;
+  // }, [user]);
 
-  if (initializing) {
-    return null;
-  }
+  // if (initializing) {
+  //   return null;
+  // }
 
-  if (!user) {
-    return <MainNavigator />;
-  } else {
-    getDataUser(firebase.auth().currentUser.email);
-    console.log(dataUser);
-    if (dataUser && dataUser.LoaiND === true) {
-      return <CustomerBottomTab />;
-    } else if (dataUser && dataUser.LoaiND === false) {
-      return (
-        <StackNavigator />
-      );
-    }
-  }
+  // if (!user) {
+  //   return <MainNavigator />;
+  // } else {
+  //   getDataUser(firebase.auth().currentUser.email);
+  //   console.log(dataUser);
+  //   if (dataUser && dataUser.LoaiND === true) {
+  //     return <CustomerBottomTab />;
+  //   } else if (dataUser && dataUser.LoaiND === false) {
+  //     return (
+  //       <StackNavigator />
+  //     );
+  //   }
+  // }
+  <SafeAreaView style = {{width: '100%', height: '100%'}}>
+    <User></User>
+  </SafeAreaView>
 
 }
 
