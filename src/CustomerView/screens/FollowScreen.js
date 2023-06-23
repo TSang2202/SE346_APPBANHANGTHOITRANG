54af2fb11d81149353232b8c2d9f06a4373dc2de
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-06-24 03:56:16 +0700

    update

--- src/CustomerView/screens/FollowScreen.js
+++ src/CustomerView/screens/FollowScreen.js
@@ -3,12 +3,13 @@ import { StyleSheet, Text, TextInput, View, Image, FlatList, TouchableOpacity } 
 import { IC_Heart, IC_Heart3X } from "../assets/icons";
 import Button from "../components/Button";
 import CUSTOM_COLOR from "../constants/colors";
-import { collection, addDoc, doc, updateDoc, onSnapshot, where } from "firebase/firestore";
+import { collection, addDoc, doc, updateDoc, onSnapshot, getDocs, where } from "firebase/firestore";
 import { async } from "@firebase/util";
 import { Firestore, firebase } from "../../../Firebase/firebase";
 import { query } from "firebase/database";
+import ProductView from "../components/ProductView";
 function FollowScreen() {
-    const [data, setdata] = useState[(null)];
+    const [data, setdata] = useState([]);
     const getDataYeuThich = async () => {
 
       const q = query(collection(Firestore, "YEUTHICH"), where("MaND", "==", firebase.auth().currentUser.uid));
@@ -24,29 +25,28 @@ function FollowScreen() {
               key: documentSnapshot.id,
           });
       });
-
-      items.map( item => {async() => {
-        const q = query(collection(Firestore, "SANPHAM"), where("MaSP", "==", item));
+      let datatest = [];
+      for(let i = 0; i < items.length; i++){
+        const q = query(collection(Firestore, "SANPHAM"), where("MaSP", "==", items[i].MaSP));
         const querySnapshot = await getDocs(q);
-
-        
+        console.log(items[i].MaSP)
         const teap = [];
         querySnapshot.forEach(documentSnapshot => {
           teap.push({
               ...documentSnapshot.data(),
               key: documentSnapshot.id,
           });
-          const newdata = data.concat(teap);
-          setdata(newdata);
-      });
-      }})
+         datatest = datatest.concat(teap);
+      })
+      }
+      setdata(datatest);
     }
 
     useEffect(() => {
       getDataYeuThich();
     }, []);
 
-    if(data == null){
+    if(data.length == 0){
       return (
         <View style = {styles.container}>
   
@@ -70,30 +70,34 @@ function FollowScreen() {
       )
     }
     else{
+      return(
       <View style={{
+        marginTop: 20,
         height: '100%',
-        width: '100%'
-    }}>
+        width: '100%',
+       
+        }}>
         <FlatList
             data={data}
             renderItem={({ item }) => {
                 return (
-                    <TouchableWithoutFeedback style={{
+                    <TouchableOpacity style={{
                         flexDirection: 'row',
-                        //justifyContent: 'space-around'
+                        justifyContent: 'space-around'
                     }}>
                         <ProductView
                             source={item.HinhAnhSP[0]}
                             title={item.TenSP}
                             price={item.GiaSP}
                         />
-                    </TouchableWithoutFeedback>
+                    </TouchableOpacity>
                 )
             }}
             numColumns={2}
         />
 
     </View>
+      )
     }
 
     
