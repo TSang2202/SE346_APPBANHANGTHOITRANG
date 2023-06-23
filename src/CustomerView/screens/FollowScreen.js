00ad7570e11c45790251a66fec93e7ff41b74839
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-06-24 03:13:38 +0700

    updateYeuThich

--- src/CustomerView/screens/FollowScreen.js
+++ src/CustomerView/screens/FollowScreen.js
@@ -1,34 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, TextInput, View, Image, FlatList, TouchableOpacity } from "react-native";
 import { IC_Heart, IC_Heart3X } from "../assets/icons";
 import Button from "../components/Button";
 import CUSTOM_COLOR from "../constants/colors";
-
+import { collection, addDoc, doc, updateDoc, onSnapshot, where } from "firebase/firestore";
+import { async } from "@firebase/util";
+import { Firestore, firebase } from "../../../Firebase/firebase";
+import { query } from "firebase/database";
 function FollowScreen() {
+    const [data, setdata] = useState[(null)];
+    const getDataYeuThich = async () => {
 
- 
+      const q = query(collection(Firestore, "YEUTHICH"), where("MaND", "==", firebase.auth().currentUser.uid));
 
-    return (
-      <View style = {styles.container}>
+      const querySnapshot = await getDocs(q);
 
-          <Image source={IC_Heart3X}
-            style ={{
-              width: 100,
-              height: 100,
+      const items = [];
+
+
+      querySnapshot.forEach(documentSnapshot => {
+          items.push({
+              ...documentSnapshot.data(),
+              key: documentSnapshot.id,
+          });
+      });
+
+      items.map( item => {async() => {
+        const q = query(collection(Firestore, "SANPHAM"), where("MaSP", "==", item));
+        const querySnapshot = await getDocs(q);
+
+        
+        const teap = [];
+        querySnapshot.forEach(documentSnapshot => {
+          teap.push({
+              ...documentSnapshot.data(),
+              key: documentSnapshot.id,
+          });
+          const newdata = data.concat(teap);
+          setdata(newdata);
+      });
+      }})
+    }
+
+    useEffect(() => {
+      getDataYeuThich();
+    }, []);
+
+    if(data == null){
+      return (
+        <View style = {styles.container}>
+  
+            <Image source={IC_Heart3X}
+              style ={{
+                width: 100,
+                height: 100,
+              }}
+            />
+  
+            <Text style = {{
+              fontSize: 17,
+              marginVertical: 10
+            }}>Your Love is empty</Text>
+            <Button
+              color = {CUSTOM_COLOR.FlushOrange}
+              title = 'SHOP NOW'
+            />
+        </View>
+        
+      )
+    }
+    else{
+      <View style={{
+        height: '100%',
+        width: '100%'
+    }}>
+        <FlatList
+            data={data}
+            renderItem={({ item }) => {
+                return (
+                    <TouchableWithoutFeedback style={{
+                        flexDirection: 'row',
+                        //justifyContent: 'space-around'
+                    }}>
+                        <ProductView
+                            source={item.HinhAnhSP[0]}
+                            title={item.TenSP}
+                            price={item.GiaSP}
+                        />
+                    </TouchableWithoutFeedback>
+                )
             }}
-          />
+            numColumns={2}
+        />
 
-          <Text style = {{
-            fontSize: 17,
-            marginVertical: 10
-          }}>Your wishist is empty</Text>
-          <Button
-            color = {CUSTOM_COLOR.FlushOrange}
-            title = 'SHOP NOW'
-          />
-      </View>
-      
-    )
+    </View>
+    }
+
+    
   }
 
 
