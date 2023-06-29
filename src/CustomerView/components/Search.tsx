6bb430a0acab1a1b0e42df97fede7ef2cb6720fe
Thương Lê Đăng <ledangthuong2003@gmail.com>
2023-06-29 14:32:59 +0700

    Xóa search

--- src/CustomerView/components/Search.tsx
+++ src/CustomerView/components/Search.tsx
@@ -1,43 +1,4 @@
-import React, { useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
-
-
-type SearchComponentProps = {
-  onSearch: (searchValue: string) => void;
-};
-
-const SearchComponent: React.FC<SearchComponentProps> = ({ onSearch }) => {
-  const [searchValue, setSearchValue] = useState('');
-
-  const handleSearch = () => {
-    onSearch(searchValue);
-  };
-
-  return (
-    <TextInput
-      style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
-      placeholder="Search"
-      value={searchValue}
-      onChangeText={setSearchValue}
-      onEndEditing={handleSearch}
-    />
-  );
-};
 
 
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    marginHorizontal: 10,
-    marginVertical: 10,
-  },
-  input: {
-    flex: 1,
-    height: 40,
-    padding: 10,
-    backgroundColor: '#eee',
-    borderRadius: 5,
-  },
-});
 
